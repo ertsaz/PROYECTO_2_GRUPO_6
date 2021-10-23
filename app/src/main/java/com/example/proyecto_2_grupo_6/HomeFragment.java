f81396494f888c5653874e08b25a4b9af6f0a91d
Ernesto Sánchez <ernesto.sanchez@utp.ac.pa>
2021-10-22 22:09:58 -0500

    Implementa la Navegación

---
+++ app/src/main/java/com/example/proyecto_2_grupo_6/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.example.proyecto_2_grupo_6;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    public static HomeFragment newInstance(String param1, String param2) {
+        HomeFragment fragment = new HomeFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
